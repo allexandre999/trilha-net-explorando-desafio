fc1b33a52c07a5054f527ef8b73e33dc7418a41d
Joao Alexandre <alexandre_efeta@hotmail.com>
2022-11-09 11:32:39 -0300

    Desafio De Hospedagem Concluído

--- Program.cs
+++ Program.cs
@@ -6,17 +6,20 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p1 = new Pessoa(nome: "João");
+Pessoa p2 = new Pessoa(nome: "Maria");
+//Implementado!!!
+Pessoa p3 = new Pessoa(nome: "Pedro");
+Pessoa p4 = new Pessoa(nome: "José");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 1, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
